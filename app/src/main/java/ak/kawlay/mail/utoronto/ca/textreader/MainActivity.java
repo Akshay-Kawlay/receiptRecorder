a29ee3982e9b4ed68cdf5a631c61145b2d27d792
Akshay Kawlay <kawlayak@gmail.com>
2018-08-14 17:17:15 -0400

    Higher quality image capture

--- app/src/main/java/ak/kawlay/mail/utoronto/ca/textreader/MainActivity.java
+++ app/src/main/java/ak/kawlay/mail/utoronto/ca/textreader/MainActivity.java
@@ -2,6 +2,8 @@ package ak.kawlay.mail.utoronto.ca.textreader;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.icu.text.SimpleDateFormat;
 import android.net.Uri;
 import android.os.Bundle;
@@ -27,6 +29,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,14 +37,12 @@ public class MainActivity extends AppCompatActivity {
     private  ImageView imageView;
     private  Bitmap imageBitmap;
     private List<String> list = new ArrayList<String>();
-    private StringBuilder csvList;
+    private StringBuilder detectedList;
     private TextView textView;
     private Intent startIntent;
-    private String mCurrentPhotoPath;
-    private Uri imageUri;
+    private HashMap<String, StringBuilder> photoPaths;
     private File imageFile;
-
-    //int PICTURE_RESULT = 1;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,11 +54,11 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView);
         textView = (TextView) findViewById(R.id.textView);
         startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+        photoPaths = new HashMap<>();
 
         snapBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                //textView.setText(" ");
                 dispatchTakePictureIntent();
             }
         });
@@ -71,25 +72,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    // static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
 
-    /*private void dispatchTakePictureIntent() {
+    private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            } catch (IOException ex){
+                //error occured
+            }
+            if (photoFile != null){
+                Uri photoURI = FileProvider.getUriForFile(this,
+                                                        "com.example.android.fileprovider",
+                                                         photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+
         }
-    }*/
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
+            Bitmap tempBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+            Bitmap rotatedBitmap = Bitmap.createBitmap(tempBitmap, 0, 0, tempBitmap.getWidth(), tempBitmap.getHeight(), matrix, true);
+            imageView.setImageBitmap(rotatedBitmap);
+            imageBitmap = rotatedBitmap;
+
         }
     }
-
 
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -97,46 +115,14 @@ public class MainActivity extends AppCompatActivity {
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
-                imageFileName,  // prefix
-                ".jpg",   // suffix
-                storageDir      // directory
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
         );
 
         // Save a file: path for use with ACTION_VIEW intents
         mCurrentPhotoPath = image.getAbsolutePath();
         return image;
-    }
-
-    static final int REQUEST_TAKE_PHOTO = 1;
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-                galleryAddPic();
-            }
-        }
-    }
-
-    private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(mCurrentPhotoPath);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
     }
 
 
@@ -155,7 +141,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
     }
 
     private void processText(FirebaseVisionText text){
@@ -169,14 +154,15 @@ public class MainActivity extends AppCompatActivity {
             list.add(txt);
 
         }
-        csvList = new StringBuilder();
+        detectedList = new StringBuilder();
         for(String s : list){
-            csvList.append(s);
-            csvList.append("\n");
+            detectedList.append(s);
+            detectedList.append("\n");
         }
-        //textView.setText(csvList.toString());
-        startIntent.putExtra("ca.utoronto.mail.kawlay.ak.textreader.SOMETHING", csvList.toString());
+
+        startIntent.putExtra("ca.utoronto.mail.kawlay.ak.textreader.SOMETHING", detectedList.toString());
         startActivity(startIntent);
+        list.clear();
 
     }
 
