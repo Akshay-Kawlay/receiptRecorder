e979f398827ee65cba9e790ec9f0c13fe8a85822
Akshay Kawlay <kawlayak@gmail.com>
2018-12-23 00:14:54 -0500

    Added manual user input for name & amount; setting monthly budget; overbudget warning

--- app/src/main/java/ak/kawlay/mail/utoronto/ca/textreader/MainActivity.java
+++ app/src/main/java/ak/kawlay/mail/utoronto/ca/textreader/MainActivity.java
@@ -1,6 +1,9 @@
 package ak.kawlay.mail.utoronto.ca.textreader;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -11,17 +14,21 @@ import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.icu.text.SimpleDateFormat;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -46,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
     private Bitmap imageBitmap;
     private Intent startIntentHistory;
     private String mCurrentPhotoPath;
+    private String mBudget = "100";
     DatabaseHelper mDatabaseHelper;
 
     @Override
@@ -56,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         Button snapBtn = findViewById(R.id.snapBtn);
         Button historyBtn = findViewById(R.id.buttonHistory);
         Button analyzeBtn = findViewById(R.id.analyzeBtn);
+        Button budgetBtn = findViewById(R.id.setBudgetBtn);
 
         imageView = findViewById(R.id.imageViewFullScreen);
         editTextCategory = findViewById(R.id.editTextCategory);
@@ -90,6 +99,48 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent startAnalyzingIntent = new Intent(getApplicationContext(), AnalysisActivity.class);
                 startActivity(startAnalyzingIntent);
+            }
+        });
+
+        final AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(this);
+        }
+
+        budgetBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                builder.setTitle("Monthly Budget");
+
+                // Set up the input
+                final EditText input = new EditText(getApplicationContext());
+                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                input.setInputType(InputType.TYPE_CLASS_NUMBER);
+                builder.setView(input);
+
+                // Set up the buttons
+                builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mBudget = input.getText().toString();
+                        SharedPreferences pref = getPreferences(MODE_PRIVATE);
+                        SharedPreferences.Editor edit = pref.edit();
+                        edit.putString("budget", mBudget);
+                        edit.commit();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+
             }
         });
     }
@@ -129,8 +180,46 @@ public class MainActivity extends AppCompatActivity {
             imageBitmap = rotatedBitmap;
 
             detectText();
-
         }
+    }
+
+    private void checkBudget(){
+        Double expense = mDatabaseHelper.getTotalExpenditureThisMonth();
+        SharedPreferences pref = getPreferences(MODE_PRIVATE);
+        String budget = pref.getString("budget", "100");
+        Double limit = Double.parseDouble(budget);
+        //Double limit = Double.parseDouble(mBudget);
+        Double warning = 0.8;
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(this);
+        }
+
+        if (expense > limit*warning && expense < limit){
+            //Toast.makeText(MainActivity.this, "Warning: Monthly Budget limit approaching", Toast.LENGTH_LONG).show();
+            builder.setTitle("Budget approaching")
+                    .setMessage("Warning: Approaching Monthly Budget limit for this month\n\nCurrent: $"+ expense.toString() + "\t\t\tLimit: $"+limit.toString())
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            //
+                        }
+                    })
+                    .show();
+        }
+        else if (expense >= limit){
+            //Toast.makeText(MainActivity.this, "Warning: Going over budget", Toast.LENGTH_LONG).show();
+            builder.setTitle("Over Budget")
+                    .setMessage("Warning: Going over budget for this month\n\nCurrent: $"+ expense.toString() + "\t\t\tLimit: $"+limit.toString())
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            //
+                        }
+                    })
+                    .show();
+        }
+
     }
 
     private File createImageFile() throws IOException {
@@ -167,30 +256,41 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void processText(FirebaseVisionText text){
+    private void processText(FirebaseVisionText text) {
         List<FirebaseVisionText.Block> blocks = text.getBlocks();
-        if (blocks.size() == 0){
+        if (blocks.size() == 0) {
             Toast.makeText(MainActivity.this, "No Text detected", Toast.LENGTH_LONG).show();
+            manualFill();
             return;
         }
 
         String maxAmount = "-1.0";
-        Rect maxAmountRect = new Rect(0,0,0,0);
+        Rect maxAmountRect = new Rect(0, 0, 0, 0);
 
         List<String> name = new ArrayList<String>();
         name.add("Unknown");
         int topMostLine = Integer.MAX_VALUE;
         int minDiff = Integer.MAX_VALUE;
         int TOTAL_position = -1;
-        Rect nameBoundingBox = new Rect(0,0,0,0);
+        boolean flag = true;
+        Rect nameBoundingBox = new Rect(0, 0, 0, 0);
 
         for (FirebaseVisionText.Block block : text.getBlocks()) {
             for (FirebaseVisionText.Line line : block.getLines()) {
                 String lineText = line.getText();
                 Rect lineFrame = line.getBoundingBox();
-                if (lineText.toUpperCase().equals("TOTAL")) {
-                    Toast.makeText(MainActivity.this, "Detected: "+lineText, Toast.LENGTH_LONG).show();
+                if (lineText.toUpperCase().startsWith("TOTAL")) {
+                    Toast.makeText(MainActivity.this, "Detected: " + lineText, Toast.LENGTH_LONG).show();
                     TOTAL_position = lineFrame.top;
+                    String tempStr[] = lineText.split(" ");
+                    if (tempStr.length > 1) {
+                        maxAmount = tempStr[1];
+                        maxAmountRect.top = lineFrame.top;
+                        maxAmountRect.bottom = lineFrame.bottom;
+                        maxAmountRect.left = lineFrame.left;
+                        maxAmountRect.right = lineFrame.right;
+                        flag = false;
+                    }
                 }
                 if (lineFrame.top < topMostLine) {
                     if (name.size() > 0) {
@@ -205,23 +305,24 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+        if (flag){
+            for (FirebaseVisionText.Block block : text.getBlocks()) {
+                for (FirebaseVisionText.Line line : block.getLines()) {
+                    String lineText = line.getText();
+                    Rect lineFrame = line.getBoundingBox();
 
-        for (FirebaseVisionText.Block block : text.getBlocks()){
-            for (FirebaseVisionText.Line line: block.getLines()) {
-                String lineText = line.getText();
-                Rect lineFrame = line.getBoundingBox();
+                    int diff = abs(lineFrame.top - TOTAL_position);
+                    if (diff < minDiff && !(lineText.toUpperCase().startsWith("TOTAL"))) {
+                        minDiff = diff;
+                        maxAmount = lineText;
 
-                int diff = abs(lineFrame.top - TOTAL_position);
-                if (diff < minDiff && !lineText.toUpperCase().equals("TOTAL")) {
-                    minDiff = diff;
-                    maxAmount = lineText;
+                        maxAmountRect.top = lineFrame.top;
+                        maxAmountRect.bottom = lineFrame.bottom;
+                        maxAmountRect.left = lineFrame.left;
+                        maxAmountRect.right = lineFrame.right;
+                    }
 
-                    maxAmountRect.top = lineFrame.top;
-                    maxAmountRect.bottom = lineFrame.bottom;
-                    maxAmountRect.left = lineFrame.left;
-                    maxAmountRect.right = lineFrame.right;
                 }
-
             }
         }
 
@@ -233,8 +334,60 @@ public class MainActivity extends AppCompatActivity {
         }catch (Exception e){
             Log.e("MainActivity: processText::", "maxAmount could not be parsed" + e.toString());
             Toast.makeText(MainActivity.this, "Could not read total amount", Toast.LENGTH_LONG).show();
+            manualFill();
         }
 
+
+    }
+
+    private void manualFill(){
+
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(this);
+        }
+
+        builder.setTitle("Add Data Manually");
+        builder.setMessage("ReceiptRecorder couldn't detect Name and Amount from the photo. Please add manually or take photo again");
+
+        Context context = getApplicationContext();
+        LinearLayout layout = new LinearLayout(context);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        // Add a TextView here for the "Title" label, as noted in the comments
+        final EditText nameBox = new EditText(context);
+        nameBox.setHint("Name");
+        layout.addView(nameBox); // Notice this is an add method
+
+        // Add another TextView here for the "Description" label
+        final EditText amountBox = new EditText(context);
+        amountBox.setHint("Amount");
+        layout.addView(amountBox); // Another add method
+
+        builder.setView(layout);
+
+        // Set up the buttons
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String input_name = "";
+                Double input_amount = -1.0;
+                String Category = editTextCategory.getText().toString();
+                input_name = nameBox.getText().toString();
+                input_amount = Double.parseDouble(amountBox.getText().toString());
+                fillRecord(input_name, input_amount, Category);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
 
     }
 
@@ -245,6 +398,7 @@ public class MainActivity extends AppCompatActivity {
         boolean check = mDatabaseHelper.addData(newRecord);
         if(check){
             Toast.makeText(MainActivity.this, "New record successfully added to database", Toast.LENGTH_LONG).show();
+            checkBudget();
         }else{
             Toast.makeText(MainActivity.this, "New record could not be added to database", Toast.LENGTH_LONG).show();
         }
